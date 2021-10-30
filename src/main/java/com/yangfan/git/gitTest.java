cb56bce0a7cd50fad25ba268a88912bb50da4a5f
cuber <2668489092@qq.com>
2021-10-30 21:02:29 +0800

    vim changed the forth time

--- src/main/java/com/yangfan/git/gitTest.java
+++ src/main/java/com/yangfan/git/gitTest.java
@@ -8,7 +8,7 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("changed in vim,hello git!");
     }
 
 }
