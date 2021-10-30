f5a5df632dad0b7154f4759b9ef6158069a80d3a
cuber <2668489092@qq.com>
2021-10-30 20:45:23 +0800

    first commit

---
+++ src/main/java/com/yangfan/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.yangfan.git;
+
+/**
+ * @Author: Cuber
+ * @Date: 2021/10/30 20:26
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
