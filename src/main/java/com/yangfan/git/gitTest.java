bce7c1720bf048bf049136f2761cccf00a41108f
cuber <2668489092@qq.com>
2021-10-30 20:47:51 +0800

    second commit

--- src/main/java/com/yangfan/git/gitTest.java
+++ src/main/java/com/yangfan/git/gitTest.java
@@ -7,6 +7,7 @@ package com.yangfan.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 
 }
