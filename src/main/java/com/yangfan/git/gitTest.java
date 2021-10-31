7311f48b659c87fdc7df1a0e530c5eef04ecbc0f
yangfan0507 <61724703+yangfan0507@users.noreply.github.com>
2021-10-31 12:05:14 +0800

    pull test
    pull test

--- src/main/java/com/yangfan/git/gitTest.java
+++ src/main/java/com/yangfan/git/gitTest.java
@@ -13,6 +13,7 @@ public class gitTest {
         System.out.println("hello git!");
         System.out.println("changed!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 
 }
